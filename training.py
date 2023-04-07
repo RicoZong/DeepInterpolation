9d4a61d820dbbff0b953cbd7f166d348af9d4442
RicoZong <94737296+RicoZong@users.noreply.github.com>
2023-04-06 22:10:37 -0400

    Update training.py

--- training.py
+++ training.py
@@ -1,70 +1,154 @@
 import os
 from deepinterpolation.generic import JsonSaver, ClassLoader
+import datetime
 import pathlib
 
 if __name__ == '__main__':
-    generator_param = {}
-    inferrence_param = {}
+    # This is used for record-keeping
+    now = datetime.datetime.now()
+    run_uid = now.strftime("%Y_%m_%d_%H_%M")
 
-    # We are reusing the data generator for training here.
-    generator_param["type"] = "generator"
-    generator_param["name"] = "SingleTifGenerator"
-    generator_param["pre_post_frame"] = 5
-    generator_param["pre_post_omission"] = 0
-    generator_param[
+    # Initialize meta-parameters objects
+    training_param = {}
+    generator_param = {}
+    network_param = {}
+    generator_test_param = {}
+
+    # An epoch is defined as the number of batches pulled from the dataset.
+    # Because our datasets are VERY large. Often, we cannot
+    # go through the entirity of the data so we define an epoch slightly
+    # differently than is usual.
+    steps_per_epoch = 5
+
+    # Those are parameters used for the Validation test generator. Here the
+    # test is done on the beginning of the data but
+    # this can be a separate file
+    generator_test_param["type"] = "generator"  # type of collection
+    generator_test_param["name"] = "SingleTifGenerator"
+    # Name of object in the collection
+    generator_test_param[
+        "pre_post_frame"
+    ] = 5  # Number of frame provided before and after the predicted frame
+    generator_test_param["train_path"] = "/Users/RicoZong/BCB430/data/seperated_images/F147_20210526_fish4_blk1_LT_9dpf_00001.tif #1.tif"
+    generator_test_param["batch_size"] = 5
+    generator_test_param["start_frame"] = 0
+    generator_test_param["end_frame"] = 100
+    generator_test_param[
+        "pre_post_omission"
+    ] = 1  # Number of frame omitted before and after the predicted frame
+    generator_test_param[
         "steps_per_epoch"
     ] = -1
-    # No steps necessary for inference as epochs are not relevant.
+    # No step necessary for testing as epochs are not relevant.
     # -1 deactivate it.
 
+    # Those are parameters used for the main data generator
+    generator_param["type"] = "generator"
+    generator_param["steps_per_epoch"] = steps_per_epoch
+    generator_param["name"] = "SingleTifGenerator"
+    generator_param["pre_post_frame"] = 5
     generator_param["train_path"] = "/Users/RicoZong/BCB430/data/seperated_images/F147_20210526_fish4_blk1_LT_9dpf_00001.tif #1.tif"
-
     generator_param["batch_size"] = 5
-    generator_param["start_frame"] = 0
-    generator_param["end_frame"] = -1  # -1 to go until the end.
-    generator_param[
-        "randomize"
-    ] = 0
-    # This is important to keep the order
-    # and avoid the randomization used during training
+    generator_param["start_frame"] = 101
+    generator_param["end_frame"] = 5000
+    generator_param["pre_post_omission"] = 0
 
-    inferrence_param["type"] = "inferrence"
-    inferrence_param["name"] = "core_inferrence"
+    # Those are parameters used for the network topology
+    network_param["type"] = "network"
+    network_param["name"] = "unet_single_1024"
+    # Name of network topology in the collection
 
-    # Replace this path to where you stored your model
-    inferrence_param[
-        "model_path"
-    ] = "/Users/RicoZong/BCB430/data/seperated_images/" +\
-        "model_B/" +\
-        "2023_04_04_13_40_unet_single_1024_mean_absolute_error_2023_04_04_13_40_model.h5"
+    # Those are parameters used for the training process
+    training_param["type"] = "trainer"
+    training_param["name"] = "core_trainer"
+    training_param["run_uid"] = run_uid
+    training_param["batch_size"] = generator_test_param["batch_size"]
+    training_param["steps_per_epoch"] = steps_per_epoch
+    training_param[
+        "period_save"
+    ] = 25
+    # network model is potentially saved
+    # during training between a regular nb epochs
+    training_param["nb_gpus"] = 1
+    training_param["apply_learning_decay"] = 0
+    training_param[
+        "nb_times_through_data"
+    ] = 1
+    # if you want to cycle through the entire data.
+    # Two many iterations will cause noise overfitting
+    training_param["learning_rate"] = 0.0001
+    training_param["pre_post_frame"] = generator_test_param["pre_post_frame"]
+    training_param["loss"] = "mean_absolute_error"
+    training_param[
+        "nb_workers"
+    ] = 1
+    # this is to enable multiple threads for data generator loading.
+    # Useful when this is slower than training
 
-    # Replace this path to where you want to store your output file
-    inferrence_param[
-        "output_file"
-    ] = "/Users/RicoZong/BCB430/data/seperated_images/model_B_result.h5"
+    training_param["model_string"] = (
+        network_param["name"]
+        + "_"
+        + training_param["loss"]
+        + "_"
+        + training_param["run_uid"]
+    )
 
-    jobdir = "/Users/RicoZong/BCB430/data/seperated_images"
+    # Where do you store ongoing training progress
+    jobdir = os.path.join(
+        "/Users/RicoZong/BCB430/data/seperated_images/model_B"
+    )
+    training_param["output_dir"] = jobdir
 
     try:
         os.mkdir(jobdir)
     except Exception:
         print("folder already exists")
 
+    # Here we create all json files that are fed to the training.
+    # This is used for recording purposes as well as input to the
+    # training process
+    path_training = os.path.join(jobdir, "training.json")
+    json_obj = JsonSaver(training_param)
+    json_obj.save_json(path_training)
+
     path_generator = os.path.join(jobdir, "generator.json")
     json_obj = JsonSaver(generator_param)
     json_obj.save_json(path_generator)
 
-    path_infer = os.path.join(jobdir, "inferrence.json")
-    json_obj = JsonSaver(inferrence_param)
-    json_obj.save_json(path_infer)
+    path_test_generator = os.path.join(jobdir, "test_generator.json")
+    json_obj = JsonSaver(generator_test_param)
+    json_obj.save_json(path_test_generator)
 
+    path_network = os.path.join(jobdir, "network.json")
+    json_obj = JsonSaver(network_param)
+    json_obj.save_json(path_network)
+
+    # We find the generator obj in the collection using the json file
     generator_obj = ClassLoader(path_generator)
-    data_generator = generator_obj.find_and_build()(path_generator)
+    generator_test_obj = ClassLoader(path_test_generator)
 
-    inferrence_obj = ClassLoader(path_infer)
-    inferrence_class = inferrence_obj.find_and_build()(path_infer,
-                                                       data_generator)
+    # We find the network obj in the collection using the json file
+    network_obj = ClassLoader(path_network)
 
-    # Except this to be slow on a laptop without GPU. Inference needs
-    # parallelization to be effective.
-    inferrence_class.run()
+    # We find the training obj in the collection using the json file
+    trainer_obj = ClassLoader(path_training)
+
+    # We build the generators object. This will, among other things,
+    # calculate normalizing parameters.
+    train_generator = generator_obj.find_and_build()(path_generator)
+    test_generator = generator_test_obj.find_and_build()(path_test_generator)
+
+    # We build the network object. This will, among other things,
+    # calculate normalizing parameters.
+    network_callback = network_obj.find_and_build()(path_network)
+
+    # We build the training object.
+    training_class = trainer_obj.find_and_build()(
+        train_generator, test_generator, network_callback, path_training
+    )
+
+    # Start training. This can take very long time.
+    training_class.run()
+
+    # Finalize and save output of the training.
+    training_class.finalize()
